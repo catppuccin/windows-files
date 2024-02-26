a9b8907050a056944791a5cc29b9b3f7d22207d1
winston <hey@winston.sh>
2024-02-26 14:44:38 +0100

    build: include color definitions to avoid JSON mess

--- install.ps1
+++ install.ps1
@@ -1,7 +1,28 @@
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 
-$palette = Invoke-WebRequest -Uri "https://github.com/catppuccin/palette/raw/v1.1.0/palette.json" | ConvertFrom-Json -AsHashtable
+$palette = [ordered]@{
+    "latte"     = @{
+        "base"   = "#eff1f5"
+        "mantle" = "#e6e9ef"
+        "crust"  = "#dce0e8"
+    }
+    "frappe"    = @{
+        "base"   = "#303446"
+        "mantle" = "#292c3c"
+        "crust"  = "#232634"
+    }
+    "macchiato" = @{
+        "base"   = "#24273a"
+        "mantle" = "#1e2030"
+        "crust"  = "#181926"
+    }
+    "mocha"     = @{
+        "base"   = "#1e1e2e"
+        "mantle" = "#181825"
+        "crust"  = "#11111b"
+    }
+}
 
 $flavorChoices = @()
 $i = 1
@@ -11,16 +32,10 @@ foreach ($flavor in $palette.Keys) {
 }
 
 $flavor = $Host.UI.PromptForChoice("Flavor", "Which flavor do you want to use?", $flavorChoices, 3)
-$flavor = $palette.Keys[$flavor]
+$colors = $palette[$flavor]
 
 $flatAppearance = $Host.UI.PromptForChoice("Appearance", "Do you want to use the flat appearance?", ('&Yes', '&No'), 1)
 $flatAppearance = $flatAppearance -eq 0
-
-# remap the palette to a K/V hashmap of the chosen flavor
-$colors = @{}
-$palette[$flavor].colors.Keys | ForEach-Object { 
-    $colors.add($_, $palette[$flavor].colors[$_].hex.replace("#", "#ff"))
-}
 
 function Update-Config {
     param ([string]$path, [hashtable]$colors)
@@ -41,30 +56,23 @@ function Update-Config {
     $config | ConvertTo-Json -Depth 10 | Set-Content -Path $path
 }
 
-$runningProcesses = @()
-$runningProcesses += Get-Process -Name "Files" -ErrorAction Ignore
-$runningProcesses += Get-Process -Name "Files.App.Server" -ErrorAction Ignore
-if ($runningProcesses.Count -gt 0) {
-    Write-Host "Killing running Files processes..." -ForegroundColor Yellow
-    $runningProcesses | ForEach-Object { $_.Kill() }
-    # wait for the processes to die
-    Start-Sleep -Seconds 2
-}
-
-$configFile = "LocalState\settings\user_settings.json"
-$configPaths = @(
+# from https://files.community/docs/contributing/updates
+$possiblePackageNames = @(
+    # dev
+    "FilesDev_ykqwq8d6ps0ag",
     # classic
-    "$env:LOCALAPPDATA\Packages\Files_wvne1zexy08sa\$configFile",
+    "Files_wvne1zexy08sa",
     # classic preview
-    "$env:LOCALAPPDATA\Packages\FilesPreview_1y0xx7n9077q4\$configFile",
+    "FilesPreview_1y0xx7n9077q4",
     # Microsoft Store
-    "$env:LOCALAPPDATA\Packages\49306atecsolution.FilesUWP_et10x9a9vyk8t\$configFile"
+    "FilesUWP_et10x9a9vyk8t"
 )
 
 $locatedPaths = @()
-foreach ($path in $configPaths) {
-    if (Test-Path $path) {
-        $locatedPaths += $path
+foreach ($name in $possiblePackageNames) {
+    $fullPath = "$env:LOCALAPPDATA\Packages\$name\LocalState\settings\user_settings.json"
+    if (Test-Path $fullPath) {
+        $locatedPaths += $fullPath
     }
 }
 
@@ -72,6 +80,18 @@ if ($locatedPaths.Count -eq 0) {
     Write-Host "Could not locate the Files configuration file." -ForegroundColor Red
     return
 }
+
+# shut down running Files processes
+$runningProcesses = @()
+$runningProcesses += Get-Process -Name "Files" -ErrorAction Ignore
+$runningProcesses += Get-Process -Name "Files.App.Server" -ErrorAction Ignore
+if ($runningProcesses.Count -gt 0) {
+    Write-Host "Killing running Files processes..." -ForegroundColor Yellow
+    $runningProcesses | ForEach-Object { $_.Kill() }
+    # wait for the processes to die
+    Start-Sleep -Seconds 3
+}
+
 
 foreach ($path in $locatedPaths) {
     Update-Config -path $path -colors $colors
